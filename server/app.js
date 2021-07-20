76cb9bc12d63ed390cdb8df093cb943588534c27
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-07-20 14:57:16 +0530

    added process terminate feature

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -14,6 +13,7 @@ app.get('/', (req, res) => {
 app.use('/system-metrics', require('./routes/os-utils'));
 app.use('/node-metrics', require('./routes/node-metrics'));
 app.use('/process-list', require('./routes/process-list'));
+app.use('/terminate', require('./routes/terminate-process'));
 
 PORT = 4000;
 app.listen(PORT, () => {
